ec5ca2dbf4309b172b69b2dfe134f709f21713ca
sihabs-dev <mdsarks123@gmail.com>
2026-04-09 00:19:10 +0600

    load word and display each word with card

--- js/script.js
+++ js/script.js
@@ -3,13 +3,44 @@ const createBtnByLevel = () => {
     .then((res) => res.json())
     .then((d) => createBtn(d.data));
 };
+
+// load word and display card start
+const loadWordLevel = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayWordByLevel(data.data));
+};
+const displayWordByLevel = (words) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  words.forEach((word) => {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <div
+          class="bg-white text-center px-3 py-6 space-y-2.5 rounded-lg shadow-lg"
+        >
+          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <p>Meaning /Pronounciation</p>
+          <p class="text-xl font-medium bangla">"${word.meaning?word.meaning:"not found"} / ${word.pronunciation}"</p>
+          <div class="flex justify-between mx-7">
+          <div><i class="fa-solid fa-circle-info"></i></div>
+          <div><i class="fa-solid fa-volume-high"></i></div>
+           
+          </div>
+        </div>
+    `;
+    cardContainer.appendChild(newDiv);
+  });
+};
+// load word end
 const createBtn = (elements) => {
   const getBtnContainer = document.getElementById("level-container");
   getBtnContainer.innerHTML = "";
   elements.forEach((element) => {
     const newDiv = document.createElement("div");
     newDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary">
+            <button onclick = "loadWordLevel(${element.level_no})" class="btn btn-outline btn-primary">
                   leason - ${element.level_no}
             </button>
         `;
